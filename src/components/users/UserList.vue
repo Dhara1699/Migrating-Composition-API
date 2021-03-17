deca8c201ff5c7c444973cb308a0eb9f224600e1
Dhara <dhara1.patel@bacancy.com>
2021-03-17 12:45:47 +0530

    Commited!

--- src/components/users/UserList.vue
+++ src/components/users/UserList.vue
@@ -1,10 +1,17 @@
 <template>
   <base-container>
     <h2>Active Users</h2>
-    <base-search @search="updateSearch" :search-term="enteredSearchTerm"></base-search>
+    <base-search
+      @search="updateSearch"
+      :search-term="enteredSearchTerm"
+    ></base-search>
     <div>
-      <button @click="sort('asc')" :class="{selected: sorting === 'asc'}">Sort Ascending</button>
-      <button @click="sort('desc')" :class="{selected: sorting === 'desc'}">Sort Descending</button>
+      <button @click="sort('asc')" :class="{ selected: sorting === 'asc' }">
+        Sort Ascending
+      </button>
+      <button @click="sort('desc')" :class="{ selected: sorting === 'desc' }">
+        Sort Descending
+      </button>
     </div>
     <ul>
       <user-item
@@ -19,6 +26,7 @@
 </template>
 
 <script>
+import { ref, computed, watch } from 'vue';
 import UserItem from './UserItem.vue';
 
 export default {
@@ -26,12 +34,77 @@ export default {
     UserItem,
   },
   props: ['users'],
-  data() {
+  emits: ['list-projects'],
+  setup(props) {
+    const enteredSearchTerm = ref('');
+    const activeSearchTerm = ref('');
+
+    const availableUsers = computed(() => {
+      let users = [];
+      if (activeSearchTerm.value) {
+        users = props.users.filter((usr) =>
+          usr.fullName.includes(activeSearchTerm.value)
+        );
+      } else if (props.users) {
+        users = props.users;
+      }
+      return users;
+    });
+
+    function updateSearch(val) {
+      enteredSearchTerm.value = val;
+    }
+
+    const sorting = ref(null);
+
+    const displayedUsers = computed(() => {
+      if (!sorting.value) {
+        return availableUsers.value;
+      }
+      return availableUsers.value.slice().sort((u1, u2) => {
+        if (sorting.value === 'asc' && u1.fullName > u2.fullName) {
+          return 1;
+        } else if (sorting.value === 'asc') {
+          return -1;
+        } else if (sorting.value === 'desc' && u1.fullName > u2.fullName) {
+          return -1;
+        } else {
+          return 1;
+        }
+      });
+    });
+
+    function sort(mode) {
+      sorting.value = mode;
+    }
+
+    watch(enteredSearchTerm, (newValue) => {
+      setTimeout(() => {
+        if (newValue === enteredSearchTerm.value) {
+          activeSearchTerm.value = newValue;
+        }
+      }, 300);
+    });
+
+    return { enteredSearchTerm,  updateSearch, displayedUsers, sorting, sort };
+  },
+};
+
+/* 
+data() {
     return {
       enteredSearchTerm: '',
       activeSearchTerm: '',
       sorting: null,
     };
+  },
+  methods: {
+    updateSearch(val) {
+      this.enteredSearchTerm = val;
+    },
+    sort(mode) {
+      this.sorting = mode;
+    },
   },
   computed: {
     availableUsers() {
@@ -62,14 +135,6 @@ export default {
       });
     },
   },
-  methods: {
-    updateSearch(val) {
-      this.enteredSearchTerm = val;
-    },
-    sort(mode) {
-      this.sorting = mode;
-    },
-  },
   watch: {
     enteredSearchTerm(val) {
       setTimeout(() => {
@@ -77,9 +142,9 @@ export default {
           this.activeSearchTerm = val;
         }
       }, 300);
-    }
+    },
   },
-};
+ */
 </script>
 
 <style scoped>
